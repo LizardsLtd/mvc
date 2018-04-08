c7253f2fa377b9afee0b22590e0f247f5381e7ed
Szymon Sasin <szymon@sasin.eu>
2018-04-08 23:04:04 +0100

    dropped collection in place of single action.

--- src/Lizards.MvcToolkit.Core/Shards/Defaults/ServicesShard.cs
+++ src/Lizards.MvcToolkit.Core/Shards/Defaults/ServicesShard.cs
@@ -1,16 +1,14 @@
 namespace Lizards.MvcToolkit.Core.Shards.Defaults
 {
   using System;
-  using System.Collections.Generic;
-  using System.Linq;
   using Microsoft.Extensions.DependencyInjection;
 
-  public sealed class ServicesShard : ConfigurableShardBase<IEnumerable<Action<IServiceCollection>>>
+  public sealed class ServicesShard : ConfigurableShardBase<Action<IServiceCollection>>
   {
-    public ServicesShard(Func<IEnumerable<Action<IServiceCollection>>> optionsFactory)
+    public ServicesShard(Func<Action<IServiceCollection>> optionsFactory)
       : base(optionsFactory) { }
 
-    protected override void ConfigureServices(IServiceCollection services, IEnumerable<Action<IServiceCollection>> options)
-      => options.ToList().ForEach(applyThis => applyThis(services));
+    protected override void ConfigureServices(IServiceCollection services, Action<IServiceCollection> options)
+      => options(services);
   }
 }
