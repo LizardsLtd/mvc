100541439960f0706cd962cc515920c3b5a6e8c1
Szymon Sasin <szymon@sasin.eu>
2018-04-08 23:34:46 +0100

    CQRS shard can cope with code without data initializastion class

--- demo/Lizards.Mvctoolkit.Demo/Startup.cs
+++ demo/Lizards.Mvctoolkit.Demo/Startup.cs
@@ -1,6 +1,7 @@
 namespace Lizards.Mvctoolkit.Demo
 {
   using Lizards.MvcToolkit.Core.Shards;
+  using Lizards.MvcToolkit.Core.Shards.Defaults;
   using Microsoft.AspNetCore.Hosting;
   using Microsoft.Extensions.Configuration;
 
@@ -8,6 +9,7 @@ namespace Lizards.Mvctoolkit.Demo
   {
     public DemoStartup(IHostingEnvironment env, IConfiguration configuration) : base(env, configuration)
     {
+      this.ApplyDefault(new CQRSShard("Lizards.Mvctoolkit.Demo"));
     }
 
     protected override string ExceptionHandlingRoute => "/home/error";
--- src/Lizards.MvcToolkit.Shards.Data/CQRSShard.cs
+++ src/Lizards.MvcToolkit.Shards.Data/CQRSShard.cs
@@ -1,63 +1,63 @@
-﻿namespace Lizards.MvcToolkit.Core.Shards.Defaults
+namespace Lizards.MvcToolkit.Core.Shards.Defaults
 {
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-    using Lizards.Data.CQRS;
-    using Lizards.Data.CQRS.DataAccess;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.DependencyInjection;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Reflection;
+  using Lizards.Data.CQRS;
+  using Lizards.Data.CQRS.DataAccess;
+  using Microsoft.AspNetCore.Builder;
+  using Microsoft.AspNetCore.Hosting;
+  using Microsoft.Extensions.DependencyInjection;
 
-    /// <summary>
-    /// Automatic registration of all CQRS required items
-    /// </summary>
-    public sealed class CQRSShard : ConfigurableShardBase<IEnumerable<string>>
+  /// <summary>
+  /// Automatic registration of all CQRS required items
+  /// </summary>
+  public sealed class CQRSShard : ConfigurableShardBase<IEnumerable<string>>
+  {
+    public CQRSShard(params string[] assembliesNames)
+        : base(assembliesNames) { }
+
+    protected override void ConfigureApp(IApplicationBuilder app, IHostingEnvironment env, IEnumerable<string> assembliesNames)
+        => app
+            .ApplicationServices
+            .GetService<IDataContextInitialiser>()
+            ?.Initialise()
+            ?.Wait();
+
+    protected override void ConfigureServices(IServiceCollection services, IEnumerable<string> assembliesNames)
     {
-        public CQRSShard(params string[] assembliesNames)
-            : base(assembliesNames) { }
-
-        protected override void ConfigureApp(IApplicationBuilder app, IHostingEnvironment env, IEnumerable<string> assembliesNames)
-            => app
-                .ApplicationServices
-                .GetService<IDataContextInitialiser>()
-                .Initialise()
-                .Wait();
-
-        protected override void ConfigureServices(IServiceCollection services, IEnumerable<string> assembliesNames)
-        {
-            assembliesNames
-                .Select(assemblyName => Assembly.Load(new AssemblyName(assemblyName)))
-                .ToList()
-                .ForEach(assembly => this.AutomaticDetection(services, assembly));
-        }
-
-        private void AutomaticDetection(IServiceCollection services, Assembly assembly)
-            => services
-                .DiscoverImplementation()
-                    .ForAssembly(assembly)
-                    .IncludeClassesOnly()
-                    .ForTypesImplementingInterface<IDataContext>()
-                    .AddAsInterface<IDataContext>()
-                .DiscoverImplementation()
-                    .ForAssembly(assembly)
-                    .IncludeClassesOnly()
-                    .ForTypesImplementingInterface<IDataContextInitialiser>()
-                    .AddAsInterface<IDataContextInitialiser>()
-                .DiscoverImplementation()
-                    .ForAssembly(assembly)
-                    .IncludeClassesOnly()
-                    .ForTypesImplementingInterface(typeof(ICommandHandler))
-                    .AddAsInterface<ICommandHandler>()
-                .DiscoverImplementation()
-                    .ForAssembly(assembly)
-                    .IncludeClassesOnly()
-                    .ForTypesImplementingInterface<IsQuery>()
-                    .AsSelf()
-                .DiscoverImplementation()
-                    .ForAssembly(assembly)
-                    .IncludeClassesOnly()
-                    .ForTypesImplementingInterface<IStory>()
-                    .AsImplementedInterfaces();
+      assembliesNames
+          .Select(assemblyName => Assembly.Load(new AssemblyName(assemblyName)))
+          .ToList()
+          .ForEach(assembly => this.AutomaticDetection(services, assembly));
     }
+
+    private void AutomaticDetection(IServiceCollection services, Assembly assembly)
+        => services
+            .DiscoverImplementation()
+                .ForAssembly(assembly)
+                .IncludeClassesOnly()
+                .ForTypesImplementingInterface<IDataContext>()
+                .AddAsInterface<IDataContext>()
+            .DiscoverImplementation()
+                .ForAssembly(assembly)
+                .IncludeClassesOnly()
+                .ForTypesImplementingInterface<IDataContextInitialiser>()
+                .AddAsInterface<IDataContextInitialiser>()
+            .DiscoverImplementation()
+                .ForAssembly(assembly)
+                .IncludeClassesOnly()
+                .ForTypesImplementingInterface(typeof(ICommandHandler))
+                .AddAsInterface<ICommandHandler>()
+            .DiscoverImplementation()
+                .ForAssembly(assembly)
+                .IncludeClassesOnly()
+                .ForTypesImplementingInterface<IsQuery>()
+                .AsSelf()
+            .DiscoverImplementation()
+                .ForAssembly(assembly)
+                .IncludeClassesOnly()
+                .ForTypesImplementingInterface<IStory>()
+                .AsImplementedInterfaces();
+  }
 }
