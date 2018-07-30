b4e28599e5d79c0fe1e4ef4aa70ac7b9f5164fc8
Szymon M Sasin <szymon.sasin@vigilantapps.com>
2018-07-30 13:41:10 +0100

    Added automatically wrapping with caching decorator

--- src/Lizards.MvcToolkit.Blocks.Data/QueriesCachingConvention.cs
+++ src/Lizards.MvcToolkit.Blocks.Data/QueriesCachingConvention.cs
@@ -1,6 +1,7 @@
 namespace Lizzards.MvcToolkit.Blocks.Data
 {
   using System;
+  using System.Collections.Generic;
   using System.Linq;
   using Lamar;
   using Lamar.Scanning;
@@ -11,35 +12,69 @@ namespace Lizzards.MvcToolkit.Blocks.Data
 
   public sealed class QueriesCachingConvention : IRegistrationConvention
   {
+    private readonly Dictionary<Type, Type> queriesMap;
     private Func<IServiceCollection, Type, Type, IServiceCollection> AddToService;
 
     public QueriesCachingConvention()
-    {
-      this.AddToService = ServiceCollectionServiceExtensions.AddTransient;
-    }
+      : this(ServiceCollectionServiceExtensions.AddTransient)
+    { }
 
     public QueriesCachingConvention(Func<IServiceCollection, Type, Type, IServiceCollection> addToService)
     {
       this.AddToService = addToService;
+
+      this.queriesMap = new Dictionary<Type, Type>
+      {
+        { typeof(IQuery<>), typeof(CachedQueryDecorator<>) },
+        { typeof(IQuery<,>), typeof(CachedQueryDecorator<,>) },
+        { typeof(IQuery<,,>), typeof(CachedQueryDecorator<,,>) },
+        { typeof(IQuery<,,,>), typeof(CachedQueryDecorator<,,,>) },
+        { typeof(IAsyncQuery<>), typeof(AsyncCachedQueryDecorator<>) },
+        { typeof(IAsyncQuery<,>), typeof(AsyncCachedQueryDecorator<,>) },
+        { typeof(IAsyncQuery<,,>), typeof(AsyncCachedQueryDecorator<,,>) },
+        { typeof(IAsyncQuery<,,,>), typeof(AsyncCachedQueryDecorator<,,,>) },
+      };
     }
 
     public void ScanTypes(TypeSet types, ServiceRegistry services)
     {
       var isQuery = typeof(IsQuery);
-      types
+      var queries = types
         .FindTypes(TypeClassification.Concretes)
         .Where(type => isQuery.IsAssignableFrom(type))
-        .ToList()
-        .ForEach(x => x.GetInterfaces().ToList().ForEach(@interface => this.AddToService(services, @interface, x)));
+        .ToList();
 
-      services.For(typeof(IQuery<>)).DecorateAllWith(typeof(CachedQueryDecorator<>));
-      services.For(typeof(IQuery<,>)).DecorateAllWith(typeof(CachedQueryDecorator<,>));
-      services.For(typeof(IQuery<,,>)).DecorateAllWith(typeof(CachedQueryDecorator<,,>));
-      services.For(typeof(IQuery<,,,>)).DecorateAllWith(typeof(CachedQueryDecorator<,,,>));
-      services.For(typeof(IAsyncQuery<>)).DecorateAllWith(typeof(AsyncCachedQueryDecorator<>));
-      services.For(typeof(IAsyncQuery<,>)).DecorateAllWith(typeof(AsyncCachedQueryDecorator<,>));
-      services.For(typeof(IAsyncQuery<,,>)).DecorateAllWith(typeof(AsyncCachedQueryDecorator<,,>));
-      services.For(typeof(IAsyncQuery<,,,>)).DecorateAllWith(typeof(AsyncCachedQueryDecorator<,,,>));
+      queries.ForEach(queryType
+        => queryType
+          .GetInterfaces()
+          .ToList()
+          .ForEach(@interface =>
+          {
+            services.AddSingleton(@interface, queryType);
+
+            this.DecorateNonGenericQueriesInterfaces(services, @interface);
+          }));
+    }
+
+    private void DecorateNonGenericQueriesInterfaces(ServiceRegistry services, Type @interface)
+    {
+      if (@interface.IsGenericType)
+      {
+        var genericVersionOfInterface = @interface.GetGenericTypeDefinition();
+        if (this.queriesMap.ContainsKey(genericVersionOfInterface))
+        {
+          var concreateDecoratorType = CreateDecorator(@interface, genericVersionOfInterface);
+          services.For(@interface).DecorateAllWith(concreateDecoratorType);
+        }
+      }
+    }
+
+    private Type CreateDecorator(Type @interface, Type genericVersionOfInterface)
+    {
+      var decoratorType = this.queriesMap[genericVersionOfInterface];
+      var genericParametes = @interface.GenericTypeArguments;
+      var concreateDecoratorType = decoratorType.MakeGenericType(genericParametes);
+      return concreateDecoratorType;
     }
   }
 }
